5ed312126d6d3b290ea832f0750d25ba6969db95
Shivani Patel <shivanipatel@MacBook-Pro-982.local>
2026-02-02 22:00:35 -0500

    Deleted the comments

--- Discussion3.py
+++ Discussion3.py
@@ -1,28 +1,22 @@
 import math
 
 class Rectangle():
-    # Create the constructor "__init__" method
 
     def __init__(self, user_width, user_height):
         self.width = user_width
         self.height = user_height
 
 
-    # Create the "__str__" method
 
     def __str__(self):
         return f"A rectangle with width {self.width} and height {self.height}"
     
 
-    # Create the "area_calculator" method
 
     def area_calculator(self):
         return self.width * self.height
 
 
-
-    # Create the "__eq__" method
-    # Returns a boolean value
 
     def __eq__(self, r1, r2):
         if r1 == r2:
@@ -35,21 +29,17 @@ class Rectangle():
 
 def main():
     r1 = Rectangle(10, 10)
-    # call the __str__ method
     print(r1)
-    # call the area_calculator method
     print("Area:", r1.area_calculator())
 
 
     r2 = Rectangle(10, 15)
     print(r2)
     print("Area:", r2.area_calculator())
-    # call the __eq__ method
     print(r1 == r2)
     print()
 
-    # you can create additional rectangle objects to 
-    # test your code or learn more about how the class behaves
+
     pass
 
 if __name__ == "__main__":
